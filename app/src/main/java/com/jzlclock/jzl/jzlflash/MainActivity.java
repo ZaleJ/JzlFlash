98ba1b6ce0c25093efcea76a1234c063f3a24ca9
JZL <947703213@qq.com>
2016-04-30 14:13:15 +0800

    Initial commit

---
+++ app/src/main/java/com/jzlclock/jzl/jzlflash/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jzlclock.jzl.jzlflash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
